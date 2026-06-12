9b5f4357e9ea6436584a505a4b6425a432d8cd17
happycuzitsme <debojyotigoswami125@gmail.com>
2026-06-12 11:29:41 +0000

    initial commit: Project sturcture and skeleton scripts

---
+++ scripts/provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Infrastructure Provisioning Script
+#Stage 2: Init skeleton
+
+set -e
+echo "===Starting provisioning==="
+echo "This is the initial skeleton. Stage 3 will add real functionality"
+
+#Will ass actual provisioning code in Stage 3
---
+++ scripts/validate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Validation script
+# Stage 2: Initial skeleton
+echo "===Running validation==="
+echo "This is the initial skeleton. Stage 6 will add real validation."
+
+exit 0
